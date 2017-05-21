a3bf02d289016ef6e43339fea84453837f945a4e
RangiBecerra <rangibecerra@gmail.com>
2017-05-21 13:32:34 -0400

    función Botones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,50 @@
 //alert("existe");
-function cerrar () {
-	alert("Cerrando");
+function cerrar01 () {
+	//alert("Cerrando");
+	document.getElementById("01").setAttribute("class", "marco01-a"); 
+}
+
+function cerrar02 () {
+	//alert("Cerrando");
+	document.getElementById("02").setAttribute("class", "marco01-a"); 
+}
+
+function cerrar03 () {
+	//alert("Cerrando");
+	document.getElementById("03").setAttribute("class", "marco01-a"); 
+}
+
+function cerrar04 () {
+	//alert("Cerrando");
+	document.getElementById("04").setAttribute("class", "marco01-a"); 
+}
+
+function restaurar() {
+	document.getElementById("01").setAttribute("class", "marco");
+	document.getElementById("02").setAttribute("class", "marco"); 
+	document.getElementById("03").setAttribute("class", "marco"); 
+	document.getElementById("04").setAttribute("class", "marco"); 
+
+}
+
+function origen () {
+	//alert("origen");
+	var elemento = document.getElementById("parrafo01");
+  	if (elemento.style.visibility != 'hidden'){
+		elemento.style.visibility = 'hidden';
+	} else {
+  		elemento.style.visibility = 'visible';
+  	}
+	//document.getElementById("parrafo01").setAttribute("class", "parrafo01-a"); 
+}
+
+function extincion() {
+	//alert("extincion");
+	var elemento = document.getElementById("parrafo02");
+  	if (elemento.style.visibility != 'hidden'){
+		elemento.style.visibility = 'hidden';
+	} else {
+  		elemento.style.visibility = 'visible';
+  	}
+	//document.getElementById("parrafo02").setAttribute("class", "parrafo02-a"); 
 }
